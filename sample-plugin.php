4e5f66ff6d7f829e030c82efd76c834abdf0585d
scribu <mail@scribu.net>
2013-02-18 15:04:11 +0200

    initial version

---
+++ sample-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Sample-plugin
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: sample-plugin
+Domain Path: /languages
+*/
+
