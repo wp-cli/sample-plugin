13a91b249671c97ce2a1f2919364deb456dafcb7
Daniel Bachhuber <daniel@handbuilt.co>
2017-03-21 06:45:48 -0700

    Fix WPCS issues

--- sample-plugin.php
+++ sample-plugin.php
@@ -1,12 +1,13 @@
 <?php
-/*
-Plugin Name: Sample-plugin
-Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
-Author: YOUR NAME HERE
-Author URI: YOUR SITE HERE
-Plugin URI: PLUGIN SITE HERE
-Text Domain: sample-plugin
-Domain Path: /languages
-*/
-
+/**
+ * Plugin Name: Sample-plugin
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: sample-plugin
+ * Domain Path: /languages
+ *
+ * @package Sample_Plugin
+ */
